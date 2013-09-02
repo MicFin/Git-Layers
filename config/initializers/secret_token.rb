f20ac17ed3bf6b7540bc1c96b516f0ffc4966946
Theodore Cleveland <tcclevela@gmail.com>
2013-09-02 16:18:05 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GitLayers::Application.config.secret_key_base = '4b5fe52d9a052f4f7ecce8b3f72cde1490c203a7e4e4202f8bf0144e49bdd47500d22444180e8acbe2a77ddc70c788967c0a481039fe4138a7304c0ad81e0ad9'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+GitLayers::Application.routes.draw do
+  root to: 'welcome#index'
+end
