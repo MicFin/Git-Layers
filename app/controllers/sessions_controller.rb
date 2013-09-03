0c28aface2ee4f249e5cc1b546a12c5db67a477e
Theodore Cleveland <tcclevela@gmail.com>
2013-09-03 10:24:13 -0400

    adds sessions routes'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,15 @@
 class SessionsController < ApplicationController
 
+	def callback
+		result = RestClient.post("https://github.com/login/oauth/access_token",
+	    {client_id: ENV['CLIENT_ID'],
+	     client_secret: ENV['CLIENT_SECRET'],
+	     code: params[:code]
+	    },{
+	     :accept => :json
+	    });
+  	puts(JSON.parse(result)['access_token'])
+	end
   # defines the session id by the user currently on the site
   # redirects to index page
 	def create
@@ -14,4 +24,5 @@ class SessionsController < ApplicationController
 		session[:user_id] = nil
 		redirect_to root_url, notice: 'Logged Out'
 	end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 GitLayers::Application.routes.draw do
   root to: 'welcome#index'
+  get 'sessions/callback', to: 'sessions#callback'
+  post 'sessions/create', to: 'sessions#create'
+  get 'sessions/destroy', to: 'sessions#destroy'
 end
