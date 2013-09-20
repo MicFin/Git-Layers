4d868c8284e9e4435267aabc4285fdef1df6a1e2
Theodore Cleveland <tcclevela@gmail.com>
2013-09-20 11:19:02 -0400

    pulls some sizing values into variables at top of Repo object

--- app/assets/javascripts/visualizations/repo.js
+++ app/assets/javascripts/visualizations/repo.js
@@ -1,6 +1,17 @@
+// Global namespace for repo-rendering and repo-fetching functions
 var Repo = {
 
+
+	grid_block_size : 60,
+	square_size: 50,
+	columns: 15,
+	canvas_width: 900,
+
+
+	// checks for no-repos, otherwise calls display functions 
+	// to put grid on page
 	initRepoLayout: function(repos) {
+
 		$(function() {
 			Repo.noReposCheck(repos);
 			Repo.repoGrid(repos);
@@ -8,7 +19,7 @@ var Repo = {
 		});
 	},
 
-
+	// displays alert on page if no repos are found for user
 	noReposCheck: function(repos) {
 		if(!repos) {
 			$('#repo-name-wrapper').remove();
@@ -16,15 +27,21 @@ var Repo = {
 			$('#repo-container-back')
 				.append('<h1> Oh Noes!</h1>')
 				.append('<h3 id="repoless-message"> You Don\'t Have any Repos! </h3>');
+				return false;
 		}
+		return true
 	},
 
+	// displays the repo grid on the page and sets event listeners for
+	// individual squares
 	repoGrid: function(repos) {
 		
-		var square_size = 60,
-			h = parseInt((repos.length / 15) + 1) * square_size;
+			h = parseInt((repos.length / Repo.columns) + 1) * Repo.grid_block_size;
 
+		// creates svg canvas
 		svg = Repo.repoCanvas(h);
+
+		// uses repos as data to create grid rectangles
 		rects = svg.selectAll('rect')
 			.data(repos)
 			.enter()
@@ -42,21 +59,22 @@ var Repo = {
 				return Color.stringColor(d['language'].toString());
 			})
 			.attr('x', function(d, i){
-				return (i % 15) * square_size;
+				return (i % Repo.columns) * Repo.grid_block_size;
 			})
 			.attr('y', function(d, i){
-				return parseInt(i / 15) * square_size;
+				return parseInt(i / Repo.columns) * Repo.grid_block_size;
 			})
 
-			.transition()
+			
+			.transition() // expands squares based randomly (within time limit)
 			.delay(function() {
 				return 500 + (Math.random() * (Math.random() + 2)) * 100;
 			})
 			.duration(function() {
 				return 500 + (Math.random() * (Math.random() + 3)) * 300;
 			})
-			.attr('height', 50)
-			.attr('width', 50)
+			.attr('height', Repo.square_size)
+			.attr('width', Repo.square_size)
 			.each('end', function() {
 				d3.select(this)
 					.on('mouseenter', function() {
@@ -64,13 +82,13 @@ var Repo = {
 						.transition()
 						.duration(50)
 						.style('fill', function(d, i){
-							return Color.stringHover(d['language'].toString());
+							return Color.stringHover(d['language'].toString()); //changes square colors
 						})
 						.transition()
 						.duration(500)
 						.attr('rx', 15);
-
-							Repo.displayRepoName($(this)[0]['__data__']['name'] + " (" + $(this)[0]['__data__']['language'] + ')');
+							Repo.displayRepoName($(this)[0]['__data__']['name'] + // displays repo name
+								" (" + $(this)[0]['__data__']['language'] + ')'); // displays repo language (main)
 					})
 
 					.on('mouseleave', function() {
@@ -78,20 +96,23 @@ var Repo = {
 						.transition()
 						.duration(1000)
 						.style('fill', function(d, i){
-							return Color.stringColor(d['language'].toString());
+							return Color.stringColor(d['language'].toString()); // resets color
 						})
 						.transition()
 						.duration(500)
 						.attr('rx', 5);
 					})
 					.on('click', function() {
-						window.open(this.__data__['html_url']);
+						window.open(this.__data__['html_url']); // opens github page on click
 					});
 			});
 	},
 
+	// sets height of container based on num repos and creates svg canvas
+	// on top of back
 	repoCanvas: function(h) {
 		
+		// sizes canvas
 		$('#repo-container-back')
 			.css('height', h + 105)
 			.css('padding-left', function() {
@@ -104,12 +125,13 @@ var Repo = {
 				'opacity': 1
 			}, 500);
 
-		var new_canvas = d3.select('#repo-container-back')
+		// creates svg canvas
+		var canvas = d3.select('#repo-container-back')
 			.append('svg')
 			.attr('height', h)
 			.attr('width', 900)
 			.attr('id','repo-container-canvas');
-		return new_canvas;
+		return canvas;
 	},
 
 	activeSortButtons: function() {
