a25cd4386cff7568d36626fa3db5dd397d481cd7
Theodore Cleveland <tcclevela@gmail.com>
2013-10-10 12:01:48 -0400

    removes puts error logging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
 	    },{
 	     :accept => :json
 	    })
-		puts result 
 		redirect_to load_user_path(access_token: JSON.parse(result)['access_token'])
 	end
 
