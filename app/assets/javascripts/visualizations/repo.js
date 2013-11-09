42d7d0fe3e54ae0c6129b1805a5067f340d8ed61
Theodore Cleveland <tcclevela@gmail.com>
2013-11-09 18:49:07 -0500

    changes title a bit

--- app/assets/javascripts/visualizations/repo.js
+++ app/assets/javascripts/visualizations/repo.js
@@ -3,7 +3,7 @@ var Repo = {
 
 	renderRepo: function(repo) {
 		$('.profile-section-header')
-			.html('<h2>' + repo["name"] + '</h2>');
+			.html('<h2>' + repo["name"] +  ' | Commits </h2>');
 		Repo.renderRepoContainer();
 		Repo.currentRepo = repo;
 		Repo.language = Repo.currentRepo['language'];
