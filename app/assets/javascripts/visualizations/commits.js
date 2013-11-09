5a469e8ee3080dd73daa176fe029340124ffd369
Theodore Cleveland <tcclevela@gmail.com>
2013-11-09 14:25:59 -0500

    starts visualization of graph-by-days

--- app/assets/javascripts/visualizations/commits.js
+++ app/assets/javascripts/visualizations/commits.js
@@ -1,5 +1,12 @@
 var Commits = {
 
+	paddingLeft: 20,
+	paddingRight: 20,
+	paddingTop: 20,
+	paddingBottom: 20,
+
+
+	// makes ajax request to get commits for repoistory
 	fetchCommits: function() {
 		var i = 0, max;
 		$.ajax({
@@ -11,14 +18,36 @@ var Commits = {
 		});
 	},
 
+	// graphs commits based on the time that the repo
+	// has been active (by days or by months)
 	graphCommits: function(commits) {
 
-		max = Commits.commitMax(commits),
-		commitDomain = Commits.commitDomain(commits),
-		w = $(window).width(),
-		h = $('#repo-container-back').height() * 0.85;
+		Commits.allCommits = commits;
+		Commits.max = Commits.commitMax(commits);
+		Commits.commitDomain = Commits.commitDomain(commits);
+		Commits.graphWidth = $(window).width();
+		Commits.graphHeight = $('#repo-container-back').height() * 0.85 + Commits.paddingBottom + Commits.paddingTop;
+		
+		Commits.graphCanvas = d3.select('#repo-container-back')
+			.append('svg')
+			.attr('id','commits-graph-canvas')
+			.attr('height', Commits.graphHeight)
+			.attr('width', Commits.graphWidth);
 
-		console.log(commitDomain);
+		dayDifference = Commits.numberOfDays(Commits.commitDomain);
+
+		if(dayDifference <= 60) {
+			Commits.graphByDays(commits)
+		}
+		console.log(Commits.commitsByDate(commits));
+
+	},
+
+	graphByMonths: function(commits) {
+		height = d3.scale.linear().domain()
+	},
+
+	graphByDays: function(commits)  {
 
 	},
 
@@ -36,23 +65,14 @@ var Commits = {
 			}
 			i+=1;
 		}
-		domain = [Commits.formatDate(first), Commits.formatDate(last)];
-		console.log(Commits.numberOfDays(domain));
-		return domain;
+		return [Commits.formatDate(first), Commits.formatDate(last)];
 	},
 
 	formatDate: function(dateString) {
 		var dateArray = dateString.split('T'),
-			date = dateArray[0].split('-'),
-			time = dateArray[1].split(':');
+			date = dateArray[0].split('-');
 
-		time[2] = time[2].split('Z')[0];
-
-		$(time).each(function(value, index) {
-			time[index] = parseInt(value, 10);
-		});
-
-		return new Date(date[0], date[1], date[2], time[0], time[1], time[2]);
+		return new Date(date[0], date[1], date[2]);
 	},
 
 	commitMax: function(sortedCommitsArray) {
@@ -60,6 +80,7 @@ var Commits = {
 			max = 0, commitsForDate = 0;
 		for(;i < length;) {
 			for(key in sortedCommitsArray[i]) {
+				console.log(key);
 				commitsForDate = sortedCommitsArray[i][key].length;
 				if(commitsForDate > max) {
 					max = commitsForDate
@@ -70,8 +91,25 @@ var Commits = {
 		return max;
 	},
 
+	commitsByDate: function(commits) {
+		var byDate = {}, i = 0, length = commits.length,
+			max = 0, date, byDateArray = [];
+
+		for(;i<length;) {
+			date = Commits.formatDate(commits[i].commit.committer.date);
+			byDate[date] = byDate[date] || [];
+			byDate[date].push(commits[i]);
+			i += 1;
+		}
+
+		
+
+		return byDate;
+
+	},
+
 	numberOfDays: function(minMaxArray) {
 		var milliseconds = minMaxArray[1] - minMaxArray[0];
-		return milliseconds* 1.15741E-8
+		return parseInt(milliseconds* 1.15741E-8, 10);
 	}
 }
