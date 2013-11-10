ef089d9324cc10014ebee9ab255da184b2739425
Theodore Cleveland <tcclevela@gmail.com>
2013-11-09 21:34:56 -0500

    working on thinning down users controller because it sucks. I can't believe I wrote this

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,3 @@
-
-
 class UsersController < ApplicationController
 	respond_to :json
 	# defines protocol for github api callback
@@ -17,48 +15,13 @@ class UsersController < ApplicationController
 
 	# loads user into databse or updates user if nonexistant or out of date
 	def load
-		github_user = Rails.cache.fetch("#{params['access_token']}", :expires_in => 9000.seconds) do 
-			JSON.parse(RestClient.get("https://api.github.com/user", {params: {:access_token => params[:access_token]}}))
+		
+		if params[:username]
+			stored_user = User.load(params[:access_token], params[:username])
+		else
+			stored_user = User.load(params[:access_token])
 		end
 
-		stored_user = User.where(github_id: github_user['id']).first
-		if stored_user
-			unless stored_user.updated_at == github_user['updated_at']
-				stored_user.update_attributes(
-					name: github_user['name'],
-					login: github_user['login'],
-					url: github_user['url'],
-					html_url: github_user['html_url'],
-					repos_url: github_user['repos_url'],
-					gists_url: github_user['gists_url'],
-					avatar_url: github_user['avatar_url'],
-					public_repos: github_user['public_repos'],
-					github_id: github_user['id'],
-					followers: github_user['followers'],
-					following: github_user['following'],
-					created_at: github_user['created_at'],
-					updated_at: github_user['updated_at'],
-					email: github_user['email']
-				)
-			end
-		else
-			stored_user = User.create(
-				name: github_user['name'],
-				login: github_user['login'],
-				url: github_user['url'],
-				html_url: github_user['html_url'],
-				repos_url: github_user['repos_url'],
-				gists_url: github_user['gists_url'],
-				avatar_url: github_user['avatar_url'],
-				public_repos: github_user['public_repos'],
-				github_id: github_user['id'],
-				followers: github_user['followers'],
-				following: github_user['following'],
-				created_at: github_user['created_at'],
-				updated_at: github_user['updated_at'],
-				email: github_user['email']
-			)
-		end
 		redirect_to create_session_path(id: stored_user.id, access_token: params[:access_token])
 	end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,67 @@
 class User < ActiveRecord::Base
 
-	
+	def self.load(access_token, options={})
+
+		if options[:username]
+			url = "https://api.github.com/users/#{options[:username]}"
+		else 
+			url = "https://api.github.com/user"
+		end
+
+		github_user_data = Rails.cache.fetch("#{access_token}", :expires_in => 9000.seconds) do 
+			JSON.parse(RestClient.get(url, {params: {:access_token => access_token}}))
+		end
+
+		stored_user = User.where(github_id: github_user_data['id']).first
+
+		if stored_user
+			stored_user = User.update_user stored_user, github_user_data
+		else
+			stored_user = User.create_user github_user_data
+		end
+
+		stored_user 
+
+	end
+
+	def self.update_user(stored_user, github_user_data)
+		unless stored_user.updated_at == github_user_data['updated_at']
+			stored_user.update_attributes(
+				name: github_user_data['name'],
+				login: github_user_data['login'],
+				url: github_user_data['url'],
+				html_url: github_user_data['html_url'],
+				repos_url: github_user_data['repos_url'],
+				gists_url: github_user_data['gists_url'],
+				avatar_url: github_user_data['avatar_url'],
+				public_repos: github_user_data['public_repos'],
+				github_id: github_user_data['id'],
+				followers: github_user_data['followers'],
+				following: github_user_data['following'],
+				created_at: github_user_data['created_at'],
+				updated_at: github_user_data['updated_at'],
+				email: github_user_data['email'])
+		end
+		stored_user
+	end
+
+	def self.create_user(github_user_data)
+		stored_user = User.create(
+			name: github_user_data['name'],
+			login: github_user_data['login'],
+			url: github_user_data['url'],
+			html_url: github_user_data['html_url'],
+			repos_url: github_user_data['repos_url'],
+			gists_url: github_user_data['gists_url'],
+			avatar_url: github_user_data['avatar_url'],
+			public_repos: github_user_data['public_repos'],
+			github_id: github_user_data['id'],
+			followers: github_user_data['followers'],
+			following: github_user_data['following'],
+			created_at: github_user_data['created_at'],
+			updated_at: github_user_data['updted_at'],
+			email: github_user_data['email'])
+	end
+
+
 end
